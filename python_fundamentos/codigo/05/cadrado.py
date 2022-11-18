9392c244b7a61e04a61e1e31188764b4df95e12b
Ricardo Big Data <ricardo.bacharelato@gmail.com>
2022-11-18 20:56:38 +0100

    Engadido tema de documentación no apartado 05_modulos_e_paquetes

---
+++ python_fundamentos/codigo/05/cadrado.py
@@ -0,0 +1,10 @@
+# Módulo cadrado.py
+"""Módulo cadrado:
+Inclúe as funcións que nos permiten obter dun cadrado:
+- o  perímetro 
+- a área 
+"""
+def perimetro(lado):
+    return (4 * lado)
+def area(lado):
+    return lado**2
